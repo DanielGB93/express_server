038c291ef321c80c900d207245b42e040c83681e
DanielGB93 <36338135+DanielGB93@users.noreply.github.com>
2023-10-07 06:42:36 +0000

    Autenticacion

--- index.js
+++ index.js
@@ -2,14 +2,56 @@ const express = require('express');
 const PORT = 3000;
 const listViewRouter = require('./list_view_router');
 const editViewRouter = require('./list_edit_router');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 //body-parser: Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 const bodyParser = require('body-parser');
+// Secret key for JWT (you should set it in your .env file)
+const SECRET_KEY = process.env.SECRET_KEY || 'secret'; 
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/list', listViewRouter);
 app.use('/edit', editViewRouter);
+
+// Ruta /login para autenticación
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Verifica las credenciales (puedes usar una base de datos real para esto)
+  if (username === 'usuario1' && password === 'contraseña1') {
+    // Crea un token JWT
+    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: 'Credenciales incorrectas' });
+  }
+});
+
+// Middleware para verificar el token JWT
+function verifyToken(req, res, next) {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(403).json({ message: 'Token no proporcionado' });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Token inválido' });
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
+
+// Ruta protegida que requiere un token JWT válido
+app.get('/rutaProtegida', verifyToken, (req, res) => {
+  res.json({ message: 'Ruta protegida exitosa', user: req.user });
+});
 
 // // // Lista de tareas (inicialmente vacía)
 // let tasks = require('./tasks.json');
